abcda3a118fce7b21f32dbca723451f7b2b4a340
lqrz <lautaro.quiroz@gmail.com>
2025-12-12 14:24:16 -0300

    files

---
+++ dotfiles/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+	-- amongst your other plugins
+	-- {'akinsho/toggleterm.nvim', version = "*", config = true}
+	-- or
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = { --[[ things you want to change go here]]
+			open_mapping = [[<c-\>]],
+			direction = "horizontal",
+			close_on_exit = true,
+		},
+	},
+}
